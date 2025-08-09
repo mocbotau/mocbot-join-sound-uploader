f643ca699b42c9f8f1e38e80d8224456e841a7c7
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-08-10 00:19:15 +1000

    feat: add theming and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import './App.css'
+import { Button } from "@/ui/button";
+
+function Navbar() {
+  return (
+    <div className="min-h-screen bg-muted">
+      <nav className="fixed top-0 h-16 bg-background border dark:border-slate-700/70 w-screen">
+        <div className="h-full flex items-center justify-between mx-auto px-4">
+          <img src={"/MOCBOT_trans.png"} alt="Logo" className="h-[70%] pb-1" />
+          <div className="flex items-center gap-3">
+            <Button variant="default" className="rounded-full hover:cursor-pointer">
+              Sign In
+            </Button>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+  );
+};
 
 export default function App() {
   return (
-    <>
-    </>
+    <Navbar />
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from "@/components/theme-provider"
 import App from './App.tsx'
+
+import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
