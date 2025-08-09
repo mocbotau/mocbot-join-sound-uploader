93a7409001aa956c2910b50a5406fbc495b8d4e7
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-08-10 01:27:40 +1000

    feat: add auth0 idp

---
+++ README.md
@@ -0,0 +1,8 @@
+# MOCBOT Join Sound Uploader
+This is a website used to interface Discord users with MOCBOT v2's join sound feature.
+
+## Setup
+### Required Environment Variables
+- `VITE_AUTH0_DOMAIN` - The auth0 application domain
+- `VITE_AUTH0_CLIENT_ID` - The auth0 application client ID
+- `VITE_AUTH0_CALLBACK_URL` - The callback URL to be used by auth0
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import { Button } from "@/ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Avatar, AvatarImage } from "./components/ui/avatar";
 
 function Navbar() {
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
   return (
-    <div className="min-h-screen bg-muted">
-      <nav className="fixed top-0 h-16 bg-background border dark:border-slate-700/70 w-screen">
+    <nav className="fixed top-0 h-16 bg-background border dark:border-slate-700/70 w-screen">
         <div className="h-full flex items-center justify-between mx-auto px-4">
           <img src={"/MOCBOT_trans.png"} alt="Logo" className="h-[70%] pb-1" />
           <div className="flex items-center gap-3">
-            <Button variant="default" className="rounded-full hover:cursor-pointer">
+          {isAuthenticated && <Avatar>
+            <AvatarImage src={user?.picture} />
+          </Avatar>}
+          {isAuthenticated ? (<Button variant="default" className="rounded-full hover:cursor-pointer" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+            Logout
+          </Button>) : (<Button variant="default" className="rounded-full hover:cursor-pointer" onClick={() => loginWithRedirect()}>
               Sign In
-            </Button>
+          </Button>)}
           </div>
         </div>
-      </nav>
-    </div>
-
+    </nav>
   );
 };
 
 export default function App() {
-  return (
+  const { user, isAuthenticated } = useAuth0();
+  return (<>
     <Navbar />
+    <div className="mt-16 p-5">
+      {
+        isAuthenticated && <>
+          <h1 className="text-4xl"> Hi, {user?.name}!</h1>
+        </>
+      }
+    </div>
+  </>
   )
 }
