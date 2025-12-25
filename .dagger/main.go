d9afa14ee003f7fe1df92dec3bba2700189dbd92
Sam Zheng <samuel.zheng1357@gmail.com>
2025-12-25 13:46:00 +1300

    feat: daggerise ci, move to Helmfile for deployment

---
+++ .dagger/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+
+	"dagger/mocbot-join-sound-uploader/internal/dagger"
+)
+
+const (
+	nodeJSVersion = "23"
+	repoName      = "mocbot-join-sound-uploader"
+)
+
+type MocbotJoinSoundUploader struct {
+	// Source code directory
+	Source *dagger.Directory
+	// +private
+	NodeVersion string
+	// +private
+	InfisicalClientSecret *dagger.Secret
+}
+
+func New(
+	// Source code directory
+	// +defaultPath="."
+	source *dagger.Directory,
+	// Infisical client secret
+	infisicalClientSecret *dagger.Secret,
+) *MocbotJoinSoundUploader {
+	return &MocbotJoinSoundUploader{
+		Source:                source,
+		NodeVersion:           nodeJSVersion,
+		InfisicalClientSecret: infisicalClientSecret,
+	}
+}
+
+// CI runs the complete CI pipeline
+func (m *MocbotJoinSoundUploader) CI(ctx context.Context) (string, error) {
+	backend := dag.NodeCi(m.Source.Directory("frontend"), dagger.NodeCiOpts{
+		NodeVersion:    m.NodeVersion,
+		PackageManager: dagger.NodeCiPackageManagerPnpm,
+	})
+
+	return backend.
+		Install().
+		WithLint().
+		Stdout(ctx)
+}
+
+// BuildAndPush builds and pushes the Docker image to the container registry
+func (m *MocbotJoinSoundUploader) BuildAndPush(
+	ctx context.Context,
+	// +default="prod"
+	env string,
+) (string, error) {
+	return dag.Docker(m.Source, m.InfisicalClientSecret, repoName, dagger.DockerOpts{
+		Environment: env,
+	}).Build().Publish(ctx)
+}
